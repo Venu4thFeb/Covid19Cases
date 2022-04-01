578ca0743e0b88c3720764aa8c4a551548400bf7
venu <evenuvasudevan@gmail.com>
2022-04-01 12:46:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ df=pd.read_csv('https://covid.ourworldindata.org/data/owid-covid-data.csv')
 df['date'] = pd.to_datetime(df['date'], format='%Y-%m-%d')
 nw_dt=df.sort_values(['date','location'],ascending=False).groupby('location',as_index=False).first()
 dt=nw_dt[nw_dt['date']!=pd.to_datetime('today').date().strftime(format='%Y-%m-%d')].head(10)
-dt.to_csv('./artifact/Top10.csv',sep='\t')
+dt.to_csv('./downloads/Top10.csv',sep='\t')
 
 lc = pd.read_csv('./countries.txt')
 #new cases, total cases, new vaccinations and total vaccinations
@@ -23,6 +23,4 @@ for country in lc:
          'new_vaccinations': sum,
          'total_vaccinations': sum
     })
-    f_dt.to_csv('./artifact/listedCountries'+time.strftime("%Y%m%d-%H%M%S")+'.csv',sep='\t',mode='a', header=False)
-
-
+    f_dt.to_csv('./downloads/listedCountries'+time.strftime("%Y%m%d-%H%M%S")+'.csv',sep='\t',mode='a', header=False)
